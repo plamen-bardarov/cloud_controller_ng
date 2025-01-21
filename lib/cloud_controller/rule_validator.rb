7db3f7140ab00141cdaeab2308f0df5382df666a
Plamen Bardarov <plamen.bardarov@sap.com>
2025-01-21 10:43:53 +0200

    removed asg destination validation

--- lib/cloud_controller/rule_validator.rb
+++ lib/cloud_controller/rule_validator.rb
@@ -35,29 +35,7 @@ module CloudController
     end
 
     def self.validate_destination(destination)
-      unless destination.index(',').nil?
-        return false unless comma_delimited_destinations_enabled?
-
-        destinations = destination.partition(',')
-        first_destination = destinations.first
-        remainder = destinations.last
-        return validate_destination(first_destination) && validate_destination(remainder)
-      end
-
-      address_list = destination.split('-')
-
-      if address_list.length == 1
-        return true if parse_ip(address_list.first)
-
-      elsif address_list.length == 2
-        ipv4s = parse_ip(address_list)
-        return false if ipv4s.nil?
-
-        sorted_ipv4s = NetAddr.sort_IPv4(ipv4s)
-        return true if ipv4s.first == sorted_ipv4s.first
-      end
-
-      false
+      true
     end
 
     def self.validate_boolean(bool)
